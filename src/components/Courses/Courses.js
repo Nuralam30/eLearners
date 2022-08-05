7bda2efaed736194b33aa3c39c2a7161ce726ccd
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-05 23:40:58 +0600

    courses displayed in the page

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Courses.css';
 
-const Courses = () => {
-    
-    fetch("./courses.json")
+
+const Courses = () =>{
+    const [courses, setCourses] = useState([]);
+
+    useEffect( () =>{
+        fetch("./courses.json")
         .then(res => res.json())
-        .then(data => {
-            const img = data[0].img;
-            console.log(img)
-        })
-
+        .then(data => setCourses(data))
+    })
+    
     return (
-        <div>
-            {/* <img src={}" alt="" /> */}
+        <div className='courses'>
+            <div className='row'>
+            {
+                courses.map(course => 
+                    <div className='col-md-3'>
+                        <div className="single-course">
+                            <img src={course.img} alt="" />
+                            <h5>{course.name}</h5>
+                            <button className='btn btn-sm btn-secondary'>enroll now</button>
+                        </div>
+                    </div>
+                )
+            }
+            </div>
         </div>
     );
 };
